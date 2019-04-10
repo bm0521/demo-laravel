5e17d87f1aaf6e1836ddbe96cfdcdb4ce618c4e5
bm0521 <bm0521.i@gmail.com>
2019-04-11 00:28:42 +0900

    認証後callback処理をコントローラーに追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,4 +49,19 @@ class LoginController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
+        /**
+     * OAuth認証の結果受け取り
+     *
+     * @param str $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        try {
+            $providerUser = \Socialite::with($provider)->user();
+        } catch(\Exception $e) {
+            return redirect('/login')->with('oauth_error', '予期せぬエラーが発生しました');
+        }
+    }
+
 }
